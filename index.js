b59dbd9b1f9a8cdbcafe78082733ad998a5896b0
cloudcome <cloudcome@163.com>
2014-11-16 16:43:43 +0800

    dato + random + typeis

---
+++ index.js
@@ -0,0 +1,16 @@
+/*!
+ * util for ydr.me
+ * @author ydr.me
+ * @create 2014-11-16 16:10
+ */
+
+'use strict';
+
+module.exports = {
+    // 数据类型
+    typeis: require('./libs/typeis.js'),
+    // 数据出口
+    dato: require('./libs/dato.js'),
+    // 随机数
+    random: require('./libs/random.js')
+};
