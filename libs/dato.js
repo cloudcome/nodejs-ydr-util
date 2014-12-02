9bf730d51814a3cfbd48df4f2d2bf468215ce370
cloudcome <cloudcome@163.com>
2014-12-02 10:43:55 +0800

    dato.removeComments

--- libs/dato.js
+++ libs/dato.js
@@ -353,3 +353,60 @@ exports.fixPath = function (p) {
 exports.toURLPath = function (p) {
     return String(p).replace(REG_URL, '/');
 };
+
+
+/**
+ * 删除注释
+ * @param str 原始字符串
+ * @returns {String}
+ */
+exports.removeComments = function (str) {
+    var currentChar;
+    var nextChar;
+    var insideString = false;
+    var insideComment = false;
+    var ret = '';
+
+    for (var i = 0; i < str.length; i++) {
+        currentChar = str[i];
+        nextChar = str[i + 1];
+
+        if (!insideComment && str[i - 1] !== '\\' && currentChar === '"') {
+            insideString = !insideString;
+        }
+
+        if (insideString) {
+            ret += currentChar;
+            continue;
+        }
+
+        if (!insideComment && currentChar + nextChar === '//') {
+            insideComment = 'single';
+            i++;
+        } else if (insideComment === 'single' && currentChar + nextChar === '\r\n') {
+            insideComment = false;
+            i++;
+            ret += currentChar;
+            ret += nextChar;
+            continue;
+        } else if (insideComment === 'single' && currentChar === '\n') {
+            insideComment = false;
+        } else if (!insideComment && currentChar + nextChar === '/*') {
+            insideComment = 'multi';
+            i++;
+            continue;
+        } else if (insideComment === 'multi' && currentChar + nextChar === '*/') {
+            insideComment = false;
+            i++;
+            continue;
+        }
+
+        if (insideComment) {
+            continue;
+        }
+
+        ret += currentChar;
+    }
+
+    return ret;
+};
